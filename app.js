c0723360f8caeccb6a54f7e918522137974963f9
Varun Ramachandran <varun.rama@gmail.com>
2017-10-14 00:02:50 -0400

    speaker transcribe test

--- app.js
+++ app.js
@@ -5,53 +5,43 @@ var port = process.env.PORT || 5000;
 var path = require('path');
 var server = require('http').createServer(app);
 var bodyParser = require('body-parser');
-var multer  = require('multer');
-var upload = multer({ dest: 'uploads/' });
+// var upload = multer({ dest: 'uploads/' });
 var fs = require('fs');
-var PythonShell = require('python-shell');
+var needle = require('needle');
+var config = require("./config.js")
 app.use(express.static('public'))
 
 //Auth ----
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '.jpg') //Appending .jpg
-  }
-})
 
 
-var upload = multer({ storage: storage });
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+
+
+//test blk
+var options = {
+  headers: 
+  { "Content-Type": "audio/flac",
+    "Transfer-Encoding": "chunked"},
+  username: config.IBM_STT_USER,
+  password: config.IBM_STT_PASS
+}
+
+
+needle.post(config.IBM_STT_URL, fs.createReadStream('audio-file.flac'), options, function(err, resp) {
+  console.dir(resp.body.results[0].alternatives)
 });
+
+
+
+
+
+
+
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname,'/index.html'));
+  
 });
 
-app.post('/upload', upload.single('image'), function (req, res, next) {
-    var filename = req.file.filename;
-    console.log("processing file " + filename);
-    var options = {
-        //pythonPath: 'C:/Python27/python.exe',
-        args: [filename.toString().substr(0,filename.toString().indexOf('.'))]
-    };
-    PythonShell.run('trumpwwe.py' , options, function (err, results) {
-        if (err) {
-            console.log(err);
-        }
-        // results is an array consisting of messages collected during execution
-        fileID = results[0];
 
-        // want to send a response of the link here
-        res.send({"file":'/gifs/'+fileID + ".gif"})
-    });
-
-});
 
 server.listen(port, function () {
   console.log("server running on port: " + port.toString())
